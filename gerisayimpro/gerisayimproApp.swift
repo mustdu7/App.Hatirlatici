69f73555c890c17441b5f82e64801f78da23d400
mustdu7 <mustdu7@gmail.com>
2026-04-07 21:51:12 +0300

    Initial Commit

---
+++ gerisayimpro/gerisayimproApp.swift
@@ -0,0 +1,17 @@
+//
+//  gerisayimproApp.swift
+//  gerisayimpro
+//
+//  Created by Mustafa on 7.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct gerisayimproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
